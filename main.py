71afbdea7c87e36cff3875650b60bcc46cf301d6
ryanhur51 <ryanhur51@gmail.com>
2024-07-07 21:43:16 -0400

    commit

--- main.py
+++ main.py
@@ -2,15 +2,23 @@ import cv2
 import numpy
 
 cam = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+gray = cv2.cvtColor(cam, cv2.COLOR_BAYER_BG2GRAY)
+faces = face_cascade.detectMultiScale(gray, 1, 4)
 
 # Loading the webcam.
-while True:
+while cam.isOpened():
     ret, frame = cam.read()
     if not ret:
         print('Error, failed to capture the frame')
-    cv2.imshow  ('Hello world', frame)
+        break
+    cv2.imshow('Eye Tracker', frame)
+
     if cv2.waitKey(1) & 0xFF == 8: # Masks the last 8 bits which are the only bits needed to recongnize the key-press. '8' represents the number for backspace. 
         break
+
+    
+    
 
 cam.release()
 cv2.destroyAllWindows()
