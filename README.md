a88f6b59cd995031b9bf588cda2f4beead5706de
ryanhur51 <ryanhur51@gmail.com>
2024-07-08 12:29:26 -0400

    i Got the eye tracker to work

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Python version: 3.11.9
 # Dependencies
 opencv version: 4.10.0.84
 numpy version: 2.0.0
- 
+
+Currently, this is process is slow and does not work all the time. I want to create an eye tracker where you can see what part of the screen you are looking at. Tracking the pupil was very slow and barely worked, so I will try a different approach. 
--- main.py
+++ main.py
@@ -2,23 +2,40 @@ import cv2
 import numpy
 
 cam = cv2.VideoCapture(0)
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-gray = cv2.cvtColor(cam, cv2.COLOR_BAYER_BG2GRAY)
-faces = face_cascade.detectMultiScale(gray, 1, 4)
+faceCascade = cv2.CascadeClassifier('eye-tracker/haarcascade_frontalface_default.xml')
+eyeCascade = cv2.CascadeClassifier('eye-tracker/haarcascade_eye_tree_eyeglasses.xml')
 
-# Loading the webcam.
+if not cam.isOpened():
+    print("Error: Unable to open the webcam.")
+    exit()
+
+# Webcam loop
 while cam.isOpened():
     ret, frame = cam.read()
-    if not ret:
-        print('Error, failed to capture the frame')
-        break
+
+    # Horizontally flip the frame
+    frame = cv2.flip(frame, 1)
+
+    # Covert the frame into grayscale
+    faceGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(faceGray, 1.1, 4)
+
+    # Face Loop
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (255, 255, 255), 5)
+        eyesGray = faceGray[y:y+h, x:x+w]
+        eyesColor = frame[y:y+h, x:x+w]
+        eyes = eyeCascade.detectMultiScale(eyesGray)
+
+        # Eyes Loop
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(eyesColor, (ex, ey), (ex + ew, ey + eh), (255, 0, 0), 5)
+
     cv2.imshow('Eye Tracker', frame)
 
+    # Exit the loop (Backspace)
     if cv2.waitKey(1) & 0xFF == 8: # Masks the last 8 bits which are the only bits needed to recongnize the key-press. '8' represents the number for backspace. 
         break
-
-    
-    
-
+ 
 cam.release()
 cv2.destroyAllWindows()
